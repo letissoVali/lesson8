5e415f55ad22674a88554249af60543b89fe276c
Iskander <valiullin00@mail.ru>
2018-09-01 14:22:52 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
 import ru.valiullin.VendingMachine;
-import ru.valiullin.VendingMachineForTea;
+import java.util.Scanner;
+//import ru.valiullin.VendingMachineForTea;
+
+
+//todo Исправить ошибку в программе «Вендинговый автомат» (проверку длинны массива перенести в другой метод).
+//todo Поэксперементировать с ключевым словом final
+//todo Задания на static будут добавлены позже
+//keep it simple или сделать проверку стоимости напитка?
+
 
 public class Main {
     public static void main(String[] args) {
-        VendingMachine vm = new VendingMachine();
-
+        Scanner scanner = new Scanner(System.in); //сканер - без объяснения
+        //заполняем" автомат напитками;
+        String[] someDrinks = new String[] {"Чай черный", "Чай зеленый", "Кофе черный", "Американо", "Капучино"};
+        VendingMachine vm = new VendingMachine(someDrinks);
         vm.addMoney(100);
-        vm.giveMeADrink(3);
+        vm.giveMeADrink(5);
 
-        VendingMachineForTea vmft = new VendingMachineForTea();
+
+
+
+        /*VendingMachineForTea vmft = new VendingMachineForTea();
         vmft.addMoney(150);
-        vmft.giveMeADrink(3);
+        vmft.giveMeADrink(3);*/
     }
 }
 
--- src/ru/valiullin/VendingMachine.java
+++ src/ru/valiullin/VendingMachine.java
@@ -2,23 +2,36 @@ package ru.valiullin;
 
 public class VendingMachine {
     private double money = 0;
-    private String[] drinks = new String[] {"Кока-кола", "Спрайт", "Фанта"};
+    private String[] drinks;
+        //конструкторы - по умолчанию и нет
+    public VendingMachine() {
+    }
+    public VendingMachine(String[] drinks) {
+        this.drinks = drinks;
+    }
 
+        //вводим денежные средства
     public void addMoney(double money) {
         this.money = money;
     }
-
+        //проверяем напиток
     protected String getDrink(int key){
-        return this.drinks[key];
+        if(key >= this.drinks.length) {
+            return "none";
+        } else {
+            return this.drinks[key];
+        }
+
     }
 
+        //выдаем напиток
     public void giveMeADrink(int key){
         if (this.money > 0) {
-            if (key > this.drinks.length-1) {
-                System.out.println("Такого напитка нет!" + this.drinks.length);
-            } else {
-                String drink = getDrink(key);
+            String drink = getDrink(key);
+            if(drink != "none"){
                 System.out.println("Возьмите ваш напиток: " + drink);
+            } else {
+                System.out.println("Такого напитка нет!" + this.drinks.length);
             }
         } else {
             System.out.println("Бесплатно не работаем!");
